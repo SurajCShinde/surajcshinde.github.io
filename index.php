461cdd56b19e357bc78dda5585bcc4ab7c1fe485
SurajCShinde <30950557+SurajCShinde@users.noreply.github.com>
2018-07-06 21:50:16 +0530

    up

--- index.php
+++ index.php
@@ -1,36 +1,43 @@
 <?php
-$to="surajchshinde@gmail.com";
-$subject="Test mail";
-$message="this is test mail";
-$headers="From:shinde.suraj924@gmail.com";
-if (mail($to,$subject,$message,$headers)) {
-echo "Thanks for mail";
-}else{
-	echo "Try again.";
+if(isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$subject = $_POST['subject'];
+	$comments = $_POST['comments'];
+	$to = "surajchshinde@gmail.com";
+	$header = "From: $name <$email>";
+	$message ="Name: $name \n \n E-mail: $email \n\n Subject: $subject\n\n Message: $comments";
+	if (mail($to,$subject,$message,$headers)) {
+	echo "Thanks for mail";
+	}else{
+		echo "Try again.";
+	}
 }
-
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>Lonavala foods</title>
-	
-</head>
-<body>
-	<p>name:<br>
-		<input type="text" name="name">
-	</p>
-	<p>E-mail:<br>
-		<input type="text" name="email">
-	</p>
-	<p>Subject:<br>
-		<input type="text" name="subject">
-	</p>
-	<p>comments:<br>
-		<textarea name="comments"></textarea>
-	</p>
-	<p><input type="submit" name="" value="Send"></p>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Lonavala foods</title>
+	</head>
+	<body>
+		<form action="" method="post" >
+			
+			<p>Name:<br>
+				<input type="text" name="name">
+			</p>
+			<p>E-mail:<br>
+				<input type="text" name="email">
+			</p>
+			<p>Subject:<br>
+				<input type="text" name="subject">
+			</p>
+			<p>comments:<br>
+				<textarea name="comments"></textarea>
+			</p>
+			<p><input type="submit" name="" value="Send"></p>
+			
+		</form>
+	</body>
 </html>
