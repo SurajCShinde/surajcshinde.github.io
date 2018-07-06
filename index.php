375f2a04db2f5ccf764efe1563cd961a371b4cee
surajchshinde <shinde.suraj924@gmail.com>
2018-07-06 21:01:25 +0530

    delete

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$to="surajchshinde@gmail.com";
+$subject="Test mail";
+$message="this is test mail";
+$headers="From:shinde.suraj924@gmail.com";
+if (mail($to,$subject,$message,$headers)) {
+echo "Thanks for mail";
+}else{
+	echo "Try again.";
+}
+
+?>
